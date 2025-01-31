00942aa44a27caeba8f775c29f8d630c4652457a
1AlexandruAndrei <117755196+1AlexandruAndrei@users.noreply.github.com>
2025-01-31 21:45:28 +0200

    Update LivePrice.vue

--- Crypto-Exchange/Frontend/src/components/coin/LivePrice.vue
+++ Crypto-Exchange/Frontend/src/components/coin/LivePrice.vue
@@ -1,52 +1,54 @@
 <template>
-    <div class="flex justify-center items-center">
-      <div class="border shadow-lg py-8 px-16 mt-10 rounded-md">
-        <FormKit type="form" 
-                :classes="{
-                    message: 'text-red-500 text-sm',
-                }"
-                :submit-attrs="{
-                    inputClass: 'border border-black py-1 px-4 rounded-md hover:bg-black hover:text-white',
-                    wrapperClass: 'border-none',
-                }"
-                @submit="getLivePrice"        
-            >
-                <FormKit type="text" name="pair" id="pair" validation="required|not:Admin" label="Pair"
-                    placeholder="BTCUSDT" 
-                    :classes="{
-                        outer: 'mb-5',
-                        label: 'block mb-1 font-bold text-sm',
-                        inner: 'w-96 border border-gray-400 rounded-md mb-1 overflow-hidden focus-within:border-blue-500',
-                        input: 'w-full h-8 px-3 border-none text-base text-gray-700 placeholder-gray-400',
-                        help: 'text-xs text-gray-500',
-                        message: 'text-red-500 text-sm'
-                    }"    
-                    v-model="formData.pair"
-                />
-        </FormKit>
-      </div>
+  <div class="flex justify-center items-center">
+    <div class="border shadow-lg py-8 px-16 mt-10 rounded-md">
+      <FormKit type="form" 
+              :classes="{
+                  message: 'text-red-500 text-sm',
+              }"
+              :submit-attrs="{
+                  inputClass: 'border border-black py-1 px-4 rounded-md hover:bg-black hover:text-white',
+                  wrapperClass: 'border-none',
+              }"
+              @submit="getLivePrice"        
+          >
+              <FormKit type="text" name="pair" id="pair" validation="required|not:Admin" label="Pair"
+                  placeholder="BTCUSDT" 
+                  :classes="{
+                      outer: 'mb-5',
+                      label: 'block mb-1 font-bold text-sm',
+                      inner: 'w-96 border border-gray-400 rounded-md mb-1 overflow-hidden focus-within:border-blue-500',
+                      input: 'w-full h-8 px-3 border-none text-base text-gray-700 placeholder-gray-400',
+                      help: 'text-xs text-gray-500',
+                      message: 'text-red-500 text-sm'
+                  }"    
+                  v-model="formData.pair"
+              />
+      </FormKit>
     </div>
+  </div>
 
-    <div>
-        <p>{{ formData.pair }} - {{ price }}</p>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import axios from 'axios'
-  import { toast } from 'vue3-toastify'
-  import 'vue3-toastify/dist/index.css'
+  <div class="flex justify-center items-center mt-10">
+      <p v-if="price" class="text-6xl text-black">{{ price }}</p>
+  </div>
+</template>
 
-  const formData = ref({
-    pair: '',
-  })
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+import { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
-  const apiBaseUrl = 'http://localhost:5009/api/Coin'
-  const price = ref(null)
+const formData = ref({
+  pair: '',
+})
 
-  async function getLivePrice() {
-    const loadingToastId = toast.loading("Loading live price...")
+const apiBaseUrl = 'http://localhost:5009/api/Coin'
+
+
+const price = ref(null)
+
+async function getLivePrice() {
+  const loadingToastId = toast.loading("Loading live price...")
   try {
     const response = await axios.get(`${apiBaseUrl}/LivePrice/${formData.value.pair}`)
     price.value = response.data;
@@ -64,10 +66,7 @@
     }
   }
 }
-  
-  </script>
-  
-  <style scoped>
-  /* Add your styles here */
-  </style>
-  
+</script>
+
+<style scoped>
+</style>
